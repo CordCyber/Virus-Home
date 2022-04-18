24ac881b5232bb0583a9be0d2707c996856c5de1
CordCyber <103395079+CordCyber@users.noreply.github.com>
2022-04-19 01:45:55 +0300

    Add files via upload

---
+++ os.py
@@ -0,0 +1,133 @@
+import os
+
+my_file = open("By CordCyber19.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber19.txt')
+
+my_file = open("By CordCyber18.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber18.txt')
+
+my_file = open("By CordCyber17.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber17.txt')
+
+my_file = open("By CordCyber16.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber16.txt')
+
+my_file = open("By CordCyber15.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber15.txt')
+
+my_file = open("By CordCyber14.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber14.txt')
+
+my_file = open("By CordCyber13.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber13.txt')
+
+my_file = open("By CordCyber12.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber12.txt')
+
+my_file = open("By CordCyber11.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber11.txt')
+
+my_file = open("By CordCyber10.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber10.txt')
+
+my_file = open("By CordCyber9.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber9.txt')
+
+my_file = open("By CordCyber8.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber8.txt')
+
+my_file = open("By CordCyber7.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber7.txt')
+
+my_file = open("By CordCyber6.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber6.txt')
+
+my_file = open("By CordCyber5.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber5.txt')
+
+my_file = open("By CordCyber4.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber4.txt')
+
+my_file = open("By CordCyber3.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber3.txt')
+
+my_file = open("By CordCyber2.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber2.txt')
+
+my_file = open("By CordCyber1.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber1.txt')
+
+my_file = open("By CordCyber1.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber1.txt')
+
+my_file = open("By CordCyber132.txt", "w+")
+my_file. write("Бом бом бом")
+my_file.close()
+
+os.startfile(r'By CordCyber132.txt')
+
+my_file = open("Комп выкл через 5 секунд).txt", "w+")
+my_file. write("Ням ням ням")
+my_file.close()
+
+os.startfile(r'By CordCyber132.txt')
